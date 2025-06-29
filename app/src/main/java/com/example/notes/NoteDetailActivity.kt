752f71f94d4842041eef82a3c6f0e965d9b03413
Sakshi <chauhansakshi4m@gmail.com>
2025-06-29 17:19:36 +0530

    modified toolbar

--- app/src/main/java/com/example/notes/NoteDetailActivity.kt
+++ app/src/main/java/com/example/notes/NoteDetailActivity.kt
@@ -4,9 +4,13 @@ import android.os.Bundle
 import android.view.MenuItem
 import android.widget.Button
 import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.TextView
 import android.widget.Toast
+import android.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import com.example.notes.R.*
 
 class NoteDetailActivity : AppCompatActivity() {
 
@@ -14,9 +18,9 @@ class NoteDetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_note_detail)
+        setContentView(layout.activity_note_detail)
 
-        setSupportActionBar(findViewById(R.id.edit_notes_toolbar))
+        setSupportActionBar(findViewById(id.edit_notes_toolbar))
 
         val noteId = intent.getIntExtra("noteId", -1)
 
@@ -25,9 +29,23 @@ class NoteDetailActivity : AppCompatActivity() {
         val factory = NotesViewModelFactory(repository)
         notesViewModel = ViewModelProvider(this, factory) [NotesViewModel::class.java]
 
+//        val toolbar = findViewById<Toolbar>(R.id.edit_notes_toolbar)
+//        setSupportActionBar(toolbar)
+        val backButton = findViewById<LinearLayout>(id.back_button_container)
+        val saveButton = findViewById<TextView>(id.toolbar_save)
+
         val titleText = findViewById<EditText>(R.id.note_title)
         val contextText = findViewById<EditText>(R.id.note_content)
-        val saveButton = findViewById<Button>(R.id.saveButton)
+
+        // toolbar actions: back and save
+        backButton.setOnClickListener {
+            finish()
+        }
+        saveButton.setOnClickListener {
+            val title = titleText.text.toString()
+            val content = contextText.text.toString()
+        }
+
 
         if (noteId != -1) {
             // load from DB if there is an existing note
@@ -59,31 +77,13 @@ class NoteDetailActivity : AppCompatActivity() {
                 notesViewModel.insert(newNote)
                 Toast.makeText(this, "Note saved!", Toast.LENGTH_SHORT).show()
             } else {
-                // update the exisiting note
+                // update the existing note
                 val updateNote = Notes(id = noteId, title = title, content = content)
                 notesViewModel.update(updateNote)
                 Toast.makeText(this, "Note Updated!", Toast.LENGTH_SHORT).show()
             }
 
             finish() // goes back to AllNotes
-        }
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
-        R.id.action_back -> {
-            // User chose the "Back" action, mark the current page as home
-            true
-        }
-
-        R.id.action_save -> {
-            // User chose the "Save" action, mark the current page as home
-            true
-        }
-
-        else -> {
-            // If we got here, the user's action was not recognized.
-            // Invoke the superclass to handle it.
-            super.onOptionsItemSelected(item)
         }
     }
 }
